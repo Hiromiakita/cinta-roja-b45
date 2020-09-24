b8051215d3277ef49f4885ea2bf8c57179a517ce
Hiromi Akita <hiromiakitao@gmail.com>
2020-09-23 22:07:20 -0500

    notas

--- 1_OBJETOS/main.js
+++ 1_OBJETOS/main.js
@@ -1,7 +1,13 @@
 
-//OBEJTO LITERAL
+//OBEJTOS LITERALEs
 
+
+//EJEMPLO 1
+//Creación de un objeto literal
+//llave : valor 
+//atributos separados con una coma entre sí
 let persona1 = {
+    //Atributos
     nombre: 'Diana',
     apellido: 'Pérez',
     edad: 21,
@@ -9,11 +15,16 @@ let persona1 = {
     comidaFav: 'chilaquiles'
 };
 
+//Mostramos todo el objeto
 console.log(persona1);
+//mostramos propiedades específicas del objeto
 console.log(persona1.nombre);
 console.log(persona1.edad);
 
 
+//--------------------------------------------------------------------------
+//EJEMPLO 2
+//Creación de un objeto literal
 let perrito = {
     //atributos
     nombre: 'Peludito',
@@ -36,21 +47,32 @@ let perrito = {
     }
 }
 
+//Mostramos todo el objeto
 console.log(perrito);
+
+//Mostramos una propiedad en específico
 console.log(perrito.cantidadDeOrejas);
 
+//Mandamos llamar los métodos de nuesstro objeto
 perrito.rodar();
 perrito.caminar();
 perrito.ladrar();
 
 
+//--------------------------------------------------------------------------
+//EJEMPLO 3
+
+
+//Creación del objeto literal
 let coche = {
+    //Atributos
     motor: 'v8',
     puertas: 6,
     color: 'morado',
     anio: 2021,
     marca: 'mazda',
 
+    //Métodos
     arrancar(){
         console.log('run runnnn');
     },
@@ -72,18 +94,28 @@ let coche = {
     }
 }
 
+
+//Mostramos todo el objeto
 console.log(coche);
+//Accedemos a su método arrancar
 coche.arrancar();
+//Cambiarmos una propiedad del objeto original usando un método
 coche.pintarCoche('verde');
+//Mostramos el objeto
 console.log(coche);
+//Cambiarmos una propiedad del objeto original usando un método
 coche.cambiarMotor('eléctrico');
+//Mostramos el objeto
 console.log(coche);
+//Cambiarmos una propiedad del objeto original usando un método
 coche.cambiarNumPuertas(2);
+//Mostramos el objeto
 console.log(coche);
 
-
+//Añadimos nuevos atributos sin modificar el original
 coche.transmision = 'automática';
 coche.frenos = 'cuando quiere';
 coche.tapiceria = 'sintética';
 coche.anio = 2030;
+//Mostramos el objeto final
 console.log(coche);
