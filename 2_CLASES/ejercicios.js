562bc06a401d9aa26a6905d9e50318d943fa0022
Hiromi Akita <hiromiakitao@gmail.com>
2020-09-24 21:22:03 -0500

    ejercicios

---
+++ 2_CLASES/ejercicios.js
@@ -0,0 +1,14 @@
+// 1.- Haz una clase llamada Persona que siga las siguientes condiciones:
+// 	 Sus atributos son: nombre, edad, RFC, género, peso y altura.
+// 	 calcularIMC()
+// 	 esMayorDeEdad()
+// 	 El constructor pide nombre, edad,género,peso y altura
+//      Generar el RFC a partir de el nombre, apellido, fechaNacimiento y sexo
+// RFC - > PRIMER LETRA DE TU NOMBRE + PRIMER LETRA DE TU APELLIDO + AÑO NACIMIENTO + MES + DÍA + PRIMER LETRA DE TU GÉNERO
+
+// 2.- Crear una clase Cuenta que tenga los siguientes atributos y métodos:
+// 	 -Titular y cantidad
+// 	 -ingresar(cantidad)
+// 	 -retirar(cantidad)
+// 	 Hacer las validaciones previas
+// 	 Como regla de negocio no debe de tener más de $900 y menos de $10
