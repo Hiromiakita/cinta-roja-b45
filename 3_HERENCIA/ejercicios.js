5583f483a8c26c3202babeb9a8dc5703dafd1bb9
Hiromi Akita <hiromiakitao@gmail.com>
2020-09-28 12:38:20 -0500

    ejercicios

--- 3_HERENCIA/ejercicios.js
+++ 3_HERENCIA/ejercicios.js
@@ -8,6 +8,54 @@
 //    también guardando un valor numérico.
 // ------------------------------------------------------------
 
+class Maestro {
+    constructor(materia, calificaciones) {
+        this.materia = materia;
+        this.calificaciones = calificaciones;
+    }
+
+    calcularPromedio() {
+        let suma = 0;
+        // [10, 9, 8, 6,10, 10]
+
+        for(let i = 0; i < this.calificaciones.length; i++) {
+           suma += this.calificaciones[i];
+        }
+
+        let promedio = suma / this.calificaciones.length;
+        console.log('promedio:'+  promedio);
+        return promedio;
+    }
+}
+
+let maestro1 = new Maestro('inglés', [10, 9, 8, 50, 100]);
+console.log(maestro1);
+maestro1.calcularPromedio();
+
+
+class MaestroFisica extends Maestro {
+    constructor(materia, calificaciones, edad) {
+        super(materia, calificaciones);
+        this.edad = edad;
+    }
+}
+
+let Antonio = new MaestroFisica('Fisica I', [100, 90, 85], 35);
+console.log(Antonio);
+Antonio.calcularPromedio();
+
+class MaestroMusica extends Maestro {
+    constructor(materia, calificaciones, antiguedad) {
+        super(materia, calificaciones);
+        this.antiguedad = antiguedad;
+    }
+}
+
+let Petra = new MaestroMusica('Música 3', [100, 100, 90], 5);
+
+console.log(Petra);
+Petra.calcularPromedio();
+
 // ------------------------------------------------------------
 // 2.- Crea una superclase llamada Electrodoméstico con las 
 //     siguientes características:
@@ -18,6 +66,15 @@
 //     c. consumoEnergetico debe iniciar con valor de 100
 // ------------------------------------------------------------
 
+class Electrodomestico {
+    constructor(precio, color, capacidad) {
+        this.precio = precio;
+        this.color = color;
+        this.capacidad = capacidad;
+        this.consumoEnergetico = 100;
+    }
+}
+
 // ------------------------------------------------------------
 // 3.- Crea una subclase de Electrodomestico llamada Lavadora 
 //     con las siguientes características:
@@ -26,6 +83,26 @@
 //     b. Crea el método precioFinal(). Este se calcula
 //        multiplicando el consumoEnergetico por la carga.
 // ------------------------------------------------------------
+
+class Lavadora extends Electrodomestico {
+    constructor(precio, color, capacidad, carga, modelo) {
+        super(precio, color, capacidad);
+        this.carga = carga;
+        this.modelo = modelo
+    }
+    precioFinal() {
+        let precio = this.consumoEnergetico * this.carga;
+        console.log(precio);
+        return precio;
+    }
+}
+
+let lavadora1 = new Lavadora(12000, 'amarilla', 20, 50, 'MABE');
+console.log(lavadora1);
+lavadora1.precioFinal();
+let lavadora2 = new Lavadora(10000, 'azul', 20, 50, 'LG');
+console.log(lavadora2);
+
 
 // ------------------------------------------------------------
 // 4.- Crear una clase Bebida que herede a dos clases Cerveza 
@@ -37,3 +114,62 @@
 // GET -> OBTENER O MOSTRAR EL VALOR DE UNA PROPIEDAD
 // SET -> CAMBIAR O ACTUALIZAR EL DE VALOR DE UNA PROPIEDAD
 // ------------------------------------------------------------
+
+class Bebida {
+    constructor(cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    getCantidad() {
+        console.log(this.cantidad);
+        return this.cantidad;
+    }
+
+    setCantidad(nuevaCantidad) {
+        this.cantidad = nuevaCantidad;
+    }
+}
+
+let michelada = new Bebida (1000);
+
+console.log(michelada);
+michelada.setCantidad(500);
+michelada.getCantidad();
+console.log(michelada);
+
+class Cerveza extends Bebida {
+    constructor(cantidad, porcentajeAlcohol) {
+        super(cantidad);
+        this.porcentajeAlcohol = porcentajeAlcohol;
+    }
+
+    getPorcentajeAlcochol (){
+        console.log(this.porcentajeAlcohol);
+        return this.porcentajeAlcohol;
+    }
+
+    setPorcentajeAlcohol (nuevoPorcentaje) {
+        this.porcentajeAlcohol = nuevoPorcentaje;
+    }
+}
+
+class Refresco extends Bebida {
+    constructor(cantidad, azucar) {
+        super(cantidad);
+        this.azucar = azucar;
+    }
+    getAzucar (){
+        console.log(this.azucar);
+        return this.azucar;
+    }
+
+    setAzucar (nuevaCantidadAzucar) {
+        this.azucar = nuevaCantidadAzucar;
+    }
+}
+
+let cocacola = new Refresco(250, 100);
+console.log(cocacola);
+cocacola.setAzucar(10);
+cocacola.getAzucar();
+console.log(cocacola);
