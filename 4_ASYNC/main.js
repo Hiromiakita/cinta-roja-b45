f802de693b64e8fc8acadf969ef53b9c3362a66e
Hiromi Akita <hiromiakitao@gmail.com>
2020-09-29 22:17:35 -0500

    async

---
+++ 4_ASYNC/main.js
@@ -0,0 +1,12 @@
+console.log(1);
+setTimeout(() => {
+    console.log(8)
+}, 0);
+setTimeout(() => {
+    console.log(5)
+}, 3000)
+console.log(10);
+setTimeout(() => {
+    console.log(2)
+}, 1000)
+console.log(4);
