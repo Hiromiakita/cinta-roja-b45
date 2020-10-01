4b46479d342540f15107769a5b731666336e0940
Hiromi Akita <hiromiakitao@gmail.com>
2020-09-30 21:01:32 -0500

    ejercicios funcional

---
+++ 5_FUNCIONAL/ejercicios.js
@@ -0,0 +1,44 @@
+// //1. Multiplicar x 5 cada uno de los elementos del siguiente arreglo -> [10, 4, 3, 2, 6, 8]
+
+
+// //2. Del arreglo siguiente, elegir los elementos que sean mayores que 5 -> [12, 2, 1, 5, 8]
+
+
+// //3. Del arreglo siguiente, elegir los elementos que sean divisibles entre 2 -> [12, 2, 1, 5, 8]
+
+
+// //4. A partir del arreglo ['Ana', 'Diego', 'Julio'] crear el siguiente -> ['1. Ana ', '2. Diego', '3. Julio']
+
+
+// //5. A partir del arreglo filtrar sólo los que sean mayores a 18;
+// //[{nombre: 'Karla', edad: 20}, 
+//     //{nombre: 'Sonia', edad: 14}, 
+//     //{nombre: 'Jesús', edad: 15},
+//     // {nombre: 'Miguel', edad: 24},
+//     // {nombre: 'Luis', edad: 12}] (editado) 
+
+//     6.- A partir del siguiente arreglo de animalitos: 
+//     const animalitos = [
+//     { nombre: "carlitos"  , especie: "conejo" },    // 0
+//     { nombre: "esteban"   , especie: "perro" },     // 1
+//     { nombre: "fabiruchis", especie: "tortuga" },   // 2
+//     { nombre: "anita"     , especie: "gato" },      // 3
+//     { nombre: "miranda"   , especie: "conejo" },    // 4
+//     { nombre: "lucas"     , especie: "conejo" },    // 5
+//     { nombre: "Horacia"   , especie: "tortuga" }    // 6
+//     ];
+// //     Genera un nuevo arreglo en el que solo se encuentren
+// //     los conejos del arreglo original.
+
+
+
+// //     7.- A partir del siguiente arreglo de mascotas:
+//     const mascotas = [
+//         { nombre: "Bimbo",    especie: "Hamster", comidasFavoritas: ["Semillas", "Nueces"] },
+//         { nombre: "Ludovico", especie: "Perro",   comidasFavoritas: ["Huesos", "Jamon"] },
+//         { nombre: "Pavlov",   especie: "Perro",   comidasFavoritas: ["Croquetas", "Pollo"] },
+//         { nombre: "Chancla",  especie: "Gato",    comidasFavoritas: ["Atun", "Pescado"] },
+//         { nombre: "Don Pepe", especie: "Perico",  comidasFavoritas: ["Semillas"] }
+//     ];
+// //     Genera un nuevo arreglo que contenga los nombres de todas 
+// //     las mascotas.
