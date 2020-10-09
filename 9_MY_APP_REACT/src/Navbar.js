60aa72fd52b2e132209000deb7d92f68c765bef2
Hiromi Akita <hiromiakitao@gmail.com>
2020-10-08 23:03:18 -0500

    primer app de react

---
+++ 9_MY_APP_REACT/src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar () {
+    return (
+        <div className="navbar">
+            <h1>SOY LA NAVBAR</h1>
+        </div>
+    )
+}
+
+export default Navbar;
