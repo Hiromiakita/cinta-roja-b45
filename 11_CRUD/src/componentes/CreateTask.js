0c5a862d8c2a1208161dc5898e2c78d87442c4d5
Hiromi Akita <hiromiakitao@gmail.com>
2020-10-14 18:58:20 -0500

    task and create task

--- 11_CRUD/src/componentes/CreateTask.js
+++ 11_CRUD/src/componentes/CreateTask.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const CreateTask = () => {
     return (
-        <div>
-            
+        <div className="card">
+            <div className="card-body">
+                <div className="form-group">
+                    <label htmlFor="title">Tarea</label>
+                    <input type="text" className="form-control" id="title" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="category">Categoría</label>
+                    <input type="text"className="form-control" id="category" />
+                </div>
+                <button className="btn btn-primary">Agregar</button>
+            </div>
         </div>
     )
 }
--- 11_CRUD/src/componentes/Task.js
+++ 11_CRUD/src/componentes/Task.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Task = () => {
     return (
-        <div>
-            
+        <div className="card">
+            <h5 className="card-header">titulo</h5>
+            <div className="card-body">
+                <h5 className="card-title">false</h5>
+                <p className="card-text">categoría</p>
+                <button className="btn btn-primary">Completar</button>
+                <button className="btn btn-danger">Eliminar</button>
+            </div>
         </div>
     )
 }
